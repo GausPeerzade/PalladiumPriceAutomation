2d88c14dbd399b12a99a7e77413df8ad115e16f9
prajwalghate <prajwalghate18@gmail.com>
2024-12-04 12:09:07 +0530

    Botnaix automation

--- Oracle.js
+++ Oracle.js
@@ -1,6 +1,6 @@
 import * as ethers from "ethers";
 import {
-    priceOracleAbi,
+    mockAggregatorAbi,
 } from "./abis/abi.js";
 
 const apiUrl = 'https://api.geckoterminal.com/api/v2/simple/networks/eth/token_price/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599';
@@ -15,29 +15,32 @@ class PriceOracle {
         let price = await response.json();
         const priceBtc = price.data.attributes.token_prices["0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"];
         // console.log(priceBtc);
-        const priceDeci = await ethers.parseEther(priceBtc);
+        const priceDeci = Math.round(parseFloat(priceBtc) * 1e8);
         // console.log(priceDeci.toString());
         const rpcUrl = "https://node.botanixlabs.dev";
-        // const rpcUrl = "http://127.0.0.1:3000/";
-        const contractAddress = "0x5BCC7cf55D3ce55cF97E05776F8155b595D40a78";
+        // const rpcUrl = "http://127.0.0.1:8545/";
+        const contractAddress = "0xc014933c805825D335e23Ef12eB92d2471D41DA7";
         const provider = new ethers.JsonRpcProvider(rpcUrl);
         const signer = new ethers.Wallet(this.harvester_private_key, provider);
-        const OracleContract = new ethers.Contract(contractAddress, priceOracleAbi, signer);
-        const gasCost = await OracleContract.setPrice.estimateGas(
-            priceDeci
-        );
-        const txResponse = await OracleContract.setPrice(
+        const OracleContract = new ethers.Contract(contractAddress, mockAggregatorAbi, signer);
+        const txResponse = await OracleContract.setLatestAnswer(
             priceDeci,
             {
-                gasLimit: gasCost
+                maxFeePerGas:7,
+                maxPriorityFeePerGas:7,
             }
         );
         const txReceipt = await txResponse.wait();
         console.log(
-            `Actual gas spent of the current transaction: ${Number(
-                txReceipt.gasUsed
+            `Gas price used: ${Number(
+                txReceipt.gasPrice
             )}`
         );
+        // console.log(
+        //     `Actual gas spent of the current transaction: ${Number(
+        //         txReceipt.gasUsed
+        //     )}`
+        // );
     }
 }
 export { PriceOracle };

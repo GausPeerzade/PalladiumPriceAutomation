3d5d801542d511cdab5ae500eb2cdcd153b635df
prajwalghate <prajwalghate18@gmail.com>
2024-12-26 23:59:26 +0530

    Cahinlink price fetch

--- Oracle.js
+++ Oracle.js
@@ -3,7 +3,6 @@ import {
     mockAggregatorAbi,
 } from "./abis/abi.js";
 
-const apiUrl = 'https://api.geckoterminal.com/api/v2/simple/networks/eth/token_price/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599';
 class PriceOracle {
     harvester_private_key;
     constructor(_harvester_private_key) {
@@ -11,36 +10,43 @@ class PriceOracle {
     }
 
     async setPrice() {
-        let response = await fetch(apiUrl);
-        let price = await response.json();
-        const priceBtc = price.data.attributes.token_prices["0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"];
-        // console.log(priceBtc);
-        const priceDeci = Math.round(parseFloat(priceBtc) * 1e8);
-        // console.log(priceDeci.toString());
-        const rpcUrl = "https://node.botanixlabs.dev";
+        const ethProvider = new ethers.JsonRpcProvider("https://arbitrum.llamarpc.com");
+        // Connect to Chainlink BTC/USD Price Feed on Arbitrum
+        const chainlinkBtcUsd = new ethers.Contract(
+            "0x6ce185860a4963106506C203335A2910413708e9",
+            mockAggregatorAbi,
+            ethProvider
+        );
+
+        const [, answer] = await chainlinkBtcUsd.latestRoundData();
+        const priceDeci = answer;
+        console.log(priceDeci.toString());
+
+        const rpcUrl = "https://rpc.ankr.com/botanix_testnet";
         // const rpcUrl = "http://127.0.0.1:8545/";
         const contractAddress = "0xc014933c805825D335e23Ef12eB92d2471D41DA7";
         const provider = new ethers.JsonRpcProvider(rpcUrl);
         const signer = new ethers.Wallet(this.harvester_private_key, provider);
         const OracleContract = new ethers.Contract(contractAddress, mockAggregatorAbi, signer);
         const txResponse = await OracleContract.setLatestAnswer(
-            priceDeci,
+            priceDeci
+            ,
             {
-                maxFeePerGas:10,
-                maxPriorityFeePerGas:10,
+                maxFeePerGas:100,
+                maxPriorityFeePerGas:100,
             }
         );
         const txReceipt = await txResponse.wait();
-        // console.log(
-        //     `Gas price used: ${Number(
-        //         txReceipt.gasPrice
-        //     )}`
-        // );
-        // console.log(
-        //     `Actual gas spent of the current transaction: ${Number(
-        //         txReceipt.gasUsed
-        //     )}`
-        // );
+        // // console.log(
+        // //     `Gas price used: ${Number(
+        // //         txReceipt.gasPrice
+        // //     )}`
+        // // );
+        // // console.log(
+        // //     `Actual gas spent of the current transaction: ${Number(
+        // //         txReceipt.gasUsed
+        // //     )}`
+        // // );
     }
 }
 export { PriceOracle };
